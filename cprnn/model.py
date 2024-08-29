21b6feb0533177e74674b6a9caba227de7cab27c
Michael Atkinson <mike@sprma.com>
2024-08-29 09:41:37 +0100

    Add pytorch module for single timestep of the RNN

--- cprnn/model.py
+++ cprnn/model.py
@@ -35,3 +35,24 @@ class CP_RNN(nn.Module):
         Y, _ = self.sstate1(X, S0)
         Y = self.sstate2(Y)
         return Y
+
+
+class CP_RNNSingleStep(CP_RNN):
+    def __init__(self, state_size, dropout=0, hidden_size=128, n_layers=2):
+        super().__init__(state_size, dropout, hidden_size, n_layers)
+
+    def forward(self, X, H):
+        """_summary_
+
+        Args:
+            X (torch.Tensor, (n, s_in_size)): Input
+            H (torch.Tensor, (s_n_layer, n, s_hs_size)): Current state
+            note n/batch axis can be ommitted from all
+
+        Return:
+            Y (torch.Tensor, (n, s_ou_size)): Output
+            H (torch.Tensor, (s_n_layer, n, s_hs_size)): New state
+        """
+        H_all, H_final = self.sstate1(X[..., None, :], H)
+        Y = self.sstate2(H_all)
+        return Y[..., 0, :], H_final
